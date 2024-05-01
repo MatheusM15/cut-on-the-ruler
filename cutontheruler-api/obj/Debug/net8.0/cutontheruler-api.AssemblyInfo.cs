614d31661c5a3ffdcf258c5be0156713366eb0dd
Matheus <matheusmenezesribeiro@hotmail.com>
2024-05-01 17:45:40 -0300

    fix

--- cutontheruler-api/obj/Debug/net8.0/cutontheruler-api.AssemblyInfo.cs
+++ cutontheruler-api/obj/Debug/net8.0/cutontheruler-api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cutontheruler-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f3fb515cde03e19d414015b34a24573aef3cd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d41ea2cf3ec31a24b316e26bd4f2b19672d4dac")]
 [assembly: System.Reflection.AssemblyProductAttribute("cutontheruler-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cutontheruler-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
